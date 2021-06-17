28902af7b6f5f4eb4cc1fb3ae2fc83726582acbd
yesliesnayder <a.kuzmickiy@innopolis.university>
2021-06-17 17:51:04 +0300

    Added examples

--- sandbox/examples/class_example_1.js
+++ sandbox/examples/class_example_1.js
@@ -1,36 +1,53 @@
-
-// Somewhere outside functions
+/**
+ * Examples of functions that should be somewhere outside
+ * and should be imported in this file
+ */
 function sprintf() {
-    let str = arguments[0];
+    let str = arguments[0].toString;
     for (let i = 1; i < arguments.length; i++) {
-        str = str.replace('%d', arguments[i]);
+        str = str.replace('%d', arguments[i].toString);
     }
-    return str;
+    return _string(str);
 }
 function stdout(str) {
-    console.log(str);
+    console.log(str.toString);
+    return this
+}
+function _int(val) {
+    this.val = val
+    this.add = function (val) {
+        return _int(this.val + val.val)
+    }
+    this.toString = this.val.toString()
+    return this
+}
+function _string(val) {
+    this.val = val
+    this.toString = this.val
+    return this
 }
 
 
 
 /**
- * The main object
+ * The main object that a parser will create
  */
 function app() {
     this.innerClass = function(a) {
         this.a = a
-        this.b = this.a + 20   // we can use function call: this.b = add(a, 20);
+        this.b = this.a.add(new _int(20))
         return this
     }
-    this.num = 10
-    this.obj = new innerClass(num)
+    this.num = new _int(10)
+    this.obj = new this.innerClass(this.num)
     stdout(
         sprintf(
-            "My number : %d\nResult : %d\n",
+            new _string("My number : %d\nResult : %d\n"),
             obj.a,
             obj.b
         )
     )
+    return this
 }
 
 app()
--- sandbox/examples/class_example_2.js
+++ sandbox/examples/class_example_2.js
@@ -1,49 +1,80 @@
-
-// Somewhere outside functions
+/**
+ * Examples of functions that should be somewhere outside
+ * and should be imported in this file
+ */
 function sprintf() {
-    let str = arguments[0];
+    let str = arguments[0].toString;
     for (let i = 1; i < arguments.length; i++) {
-        str = str.replace('%d', arguments[i]);
+        str = str.replace('%d', arguments[i].toString);
     }
-    return str;
+    return _string(str);
 }
 function stdout(str) {
-    console.log(str);
+    console.log(str.toString);
+    return this
 }
-function get(arr, index) {
-    return arr[index]
+function _int(val) {
+    this.val = val
+    this.add = function (val) {
+        return new _int(this.val + val.val)
+    }
+    this.mul = function (val) {
+        return new _int(this.val * val.val)
+    }
+    this.sub = function (val) {
+        return new _int(this.val - val.val)
+    }
+    this.toString = this.val.toString()
+    return this
+}
+function _array(args) {
+    this.val = args
+    this.get = function (index) {
+        return this.val[index.val]
+    }
+    return this
+}
+function _string(val) {
+    this.val = val
+    this.toString = this.val
+    return this
 }
 
 
 
+/**
+ * The main object that a parser will create
+ */
 function app() {
     this.innerClass = function(a, b) {
         this.a = a
         this.b = b
-        this.first = a + b
+        this.first = this.a.add(this.b)
         this.innerInnerClass = function(c) {
             this.c = c
-            this.second = c * b
+            // there is ^, EOLANG does not support same names inside objects
+            // therefore it would be better to use 'this.b'
+            this.second = this.c.mul(this.b)
             return this
         }
         return this
     }
     this.someClass = function() {
         this.innerInnerClass = function(...args) {
-            this.args = args
-            this.second = get(args, 0) - 15
+            this.args = new _array(args)
+            this.second = this.args.get(new _int(0)).sub(new _int(15))
             return this
         }
         return this
     }
-    this.num1 = 10
-    this.num2 = 20
-    this.s = this.innerClass(this.num1, this.num2)
+    this.num1 = new _int(10)
+    this.num2 = new _int(20)
+    this.s = new this.innerClass(this.num1, this.num2)
     this.inner1 = this.s.innerInnerClass(this.num1).second
     this.inner2 = this.someClass().innerInnerClass(this.num1).second
     stdout(
         sprintf(
-            "Numbers: %d, %d\nAddition: %d\nMultiplication: %d\nSubtraction (Wrong): %d\n",
+            new _string("Numbers: %d, %d\nAddition: %d\nMultiplication: %d\nSubtraction (Wrong): %d\n"),
             this.num1,
             this.num2,
             this.s.first,
@@ -51,6 +82,7 @@ function app() {
             this.inner2
         )
     )
+    return this
 }
 
 app()
