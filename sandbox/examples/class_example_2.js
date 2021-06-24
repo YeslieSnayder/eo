c328ed6eb3540bc9aafdd56901581224dd7bf9f0
Max <max.pengwyn@gmail.com>
2021-06-24 15:13:05 +0300

    added lib

--- sandbox/examples/class_example_2.js
+++ sandbox/examples/class_example_2.js
@@ -40,7 +40,7 @@ _int.prototype = _object.prototype
 
 function _array(args) {
     _object.call(this, function () {return args})
-    this.val = args
+    this.val = function () {return args}
     this.get = function (index) {
         return this.val[index.val]
     }
@@ -63,35 +63,35 @@ _string.prototype = _object.prototype
  */
 function app() {
     this.innerClass = function(a, b) {
-        this.a = a
-        this.b = b
-        this.first = this.a.add(this.b)
+        this.a = function () { return a}
+        this.b = function () {return b}
+        this.first = function () {return this.a().add(this.b())}
         this.innerInnerClass = function(c) {
-            this.c = c
-            this.second = this.c.mul(this.b)
+            this.c = function () {return c}
+            this.second = function () {return this.c.mul(this.b)}
             return this
         }
         return this
     }
     this.someClass = function() {
         this.innerInnerClass = function(...args) {
-            this.args = new _array(args)
-            this.second = this.args.get(new _int(0)).sub(new _int(15))
+            this.args = function () {return new _array(args)}
+            this.second = function () {return this.args.get(new _int(0)).sub(new _int(15))}
             return this
         }
         return this
     }
-    this.num1 = new _int(10)
-    this.num2 = new _int(20)
-    this.s = new this.innerClass(this.num1, this.num2)
-    this.inner1 = this.s.innerInnerClass(this.num1).second
-    this.inner2 = this.someClass().innerInnerClass(this.num1).second
+    this.num1 = function () {return new _int(10)}
+    this.num2 = function () {return new _int(20)}
+    this.s = function () {return new this.innerClass(this.num1(), this.num2())}
+    this.inner1 = function () {return this.s().innerInnerClass(this.num1()).second}
+    this.inner2 = function () {return this.someClass().innerInnerClass(this.num1).second}
     stdout.call(this,
         sprintf(
             new _string("Numbers: %d, %d\nAddition: %d\nMultiplication: %d\nSubtraction (Wrong): %d\n"),
             this.num1,
             this.num2,
-            this.s.first,
+            this.s().first,
             this.inner1,
             this.inner2
         )
