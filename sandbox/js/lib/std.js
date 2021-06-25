8e73ff413039f2c2ecb802e17729b665b506a773
yesliesnayder <YeslieSnayder>
2021-06-25 16:50:22 +0300

    Updated library

--- sandbox/js/lib/std.js
+++ sandbox/js/lib/std.js
@@ -19,7 +19,7 @@ function stdout(str) {
 }
 
 function _array(args) {
-    this._object.call(this, function () {
+    _object.call(this, function () {
         return args
     })
     this.val = args
@@ -28,34 +28,48 @@ function _array(args) {
     }
 
     this.empty = function () {
-        return args.empty()
+        return new _bool(args.empty())
     }
 
     this.length = function () {
-        return args.length()
+        return new _numericalVal(args.length())
     }
 
     this.append = function (val) {
-        return args.add(val)
+        args.push(val)
+        return this
     }
 
-    this.map = function (ele, index) {
-
-    }
+    this.map = function (f) {}
+    this.reduce = function (a, f) {}
+    this.each = function (f) {}
+    this.mapi = function (f) {}
     return this
 }
 
 function _string(val) {
-    this._object.call(this, function () {
+    _object.call(this, function () {
         return val
     })
     this.val = val
     this.toString = this.val
+
+    this.trim = function () {
+        return new _string(this.val.trim())
+    }
+
+    this.toInt = function () {
+        return new _numericalVal(Number(this.val))
+    }
+
+    this.eq = function (str) {
+        return new _bool(this.val === str.val)
+    }
     return this
 }
 
 function _numericalVal(val) {
-    this._object.call(this, val)
+    _object.call(this, val)
     this.val = val
     this.add = function (val) {
         return new _numericalVal(this.val + val.val)
@@ -68,14 +82,15 @@ function _numericalVal(val) {
     }
 
     this.eq = function (val) {
-        return new _numericalVal(this.val === val.val)
+        return new _bool(this.val === val.val)
     }
 
     this.greater = function (val) {
-        return new _numericalVal(this.val > val.val)
+        return new _bool(this.val > val.val)
     }
+
     this.geq = function (val) {
-        return new _numericalVal(this.val >= val.val)
+        return new _bool(this.val >= val.val)
     }
 
     this.neg = function () {
@@ -83,11 +98,11 @@ function _numericalVal(val) {
     }
 
     this.less = function (val) {
-        return new _numericalVal(this.val < val.val)
+        return new _bool(this.val < val.val)
     }
 
     this.leq = function (val){
-        return new _numericalVal(this.val <= val.val)
+        return new _bool(this.val <= val.val)
     }
 
     this.pow = function (val) {
@@ -111,12 +126,96 @@ function _numericalVal(val) {
     return this
 }
 
-function _bool() {
+function _bool(val) {
+    _object.call(this, function () {return val})
+    this.val = val
+
+    this.if = function (if_true, if_false) {
+        return this.val ? if_true : if_false
+    }
+
+    this.while = function (action) {
+        return new _object(new function () {
+            while (val) {
+                action()
+            }})
+    }
+
+    this.not = function () {
+        return new _bool(!this.val)
+    }
+
+    this.and = function (...bool_objs) {
+        let res = this.val
+        for (let c in bool_objs) {
+            res = res && c.val
+            if (!res) break
+        }
+        return new _bool(res)
+    }
+
+    this.or = function (...bool_objs) {
+        let res = this.val
+        for (let c in bool_objs) {
+            res = res || c.val
+            if (res) break
+        }
+        return new _bool(res)
+    }
+
+    this.toString = this.val.toString()
+    return this
 }
 
+function _char(val) {
+    _object.call(this, function () {return val})
+    this.val = val
+
+    this.toString = function () {
+        return new _string(this.val)
+    }
+
+    this.toString = this.val
+    return this
+}
+
+function _seq(...steps) {
+    _object.call(this, function () {
+        for (let i = 0; i < steps.length; i++) {
+            if (typeof steps[i]._instance !== 'undefined')
+                steps[i]._instance()
+        }
+    })
+    this.val = steps
+    return this
+}
+
+function _random() {return this}
+
+function _error(msg) {
+    this.val = msg
+    return this
+}
+
+function _regex(val) {
+    this.val = val
+    this.match = function (txt) {}  // return Array of matched strings
+    this.matches = function (txt) {}
+    return this
+}
+
+
+
 module.exports = {
-    _object: _object,
+    _seq: _seq,
+    _bool: _bool,
+    _char: _char,
     _array: _array,
+    _error: _error,
+    _regex: _regex,
+    stdout: stdout,
     _string: _string,
+    sprintf: sprintf,
+    _random: _random,
     _numericalVal: _numericalVal
 }
